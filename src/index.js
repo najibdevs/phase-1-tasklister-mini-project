2d0507f14c20b0e4e2289d8185f4d69cb3fa1515
najibdevs <najibsoftwareeng@gmail.com>
2024-04-12 21:13:15 +0300

    tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const taskInput = document.getElementById('new-task-description');
+      const taskDescription = taskInput.value;
+
+      const taskItem = document.createElement('li');
+
+      taskItem.textContent = taskDescription;
+
+      const taskList = document.getElementById('tasks');
+      taskList.appendChild(taskItem);
+
+      taskInput.value = '';
+  });
 });
